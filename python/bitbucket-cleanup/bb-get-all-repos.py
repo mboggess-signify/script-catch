f2ca3ce218f6d4a7248c5d4e06a1829c82e213ba
Megan Boggess <mboggess@signifyhealth.com>
2023-03-30 16:33:30 -0400

    Able to get all repos

--- python/bitbucket-cleanup/bb-get-all-repos.py
+++ python/bitbucket-cleanup/bb-get-all-repos.py
@@ -11,6 +11,10 @@ import logging
 from logging import critical, error, info, warning, debug
 import requests
 from requests.auth import HTTPBasicAuth
+import json
+import atlassian
+from atlassian import Bitbucket
+from pprint import pprint
 
 # Define functions here
 
@@ -46,14 +50,85 @@ def parse_arguments():
 
     return args
 
+# Function to log in to Bitbucket instance
+def bitbucket_login(baseURL, username, password):
+    bitbucket = Bitbucket(
+        url = baseURL,
+        username = username,
+        password = password)
+
+    return bitbucket
+
+# Function to get all projects
+def get_projects(baseURL, username, password):
+    project_list = []
+    try:
+        bitbucket = bitbucket_login(baseURL, username, password)
+    except Exception as err:
+        debug(f"Unexpected {err=}, {type(err)=}")
+        sys.exit()
+
+    try:
+        #pprint(list(bitbucket.project_list()))
+        #pprint(bitbucket.project("VHRA"))
+
+        for project in bitbucket.project_list():
+            #print(str(project['key']))
+            project_list.append(str(project['key']))
+    except Exception as err:
+        debug(f"Unexpected {err=}, {type(err)=}")
+        sys.exit()
+
+    return project_list
+
+# Function to get repos from a project
+def get_repos(baseURL, username, password, projectKey):
+    repo_list = []
+    try:
+        bitbucket = bitbucket_login(baseURL, username, password)
+    except Exception as err:
+        debug(f"Unexpected {err=}, {type(err)=}")
+        sys.exit()
+
+    try:
+        #pprint(list(bitbucket.repo_list(projectKey)))
+        for repo in bitbucket.repo_list(projectKey):
+            repo_list.append(str(repo['slug']))
+    except Exception as err:
+        debug(f"Unexpected {err=}, {type(err)=}")
+        sys.exit()
+
+    return repo_list
+
 def main():
     pass
 
     username = args.username
     password = args.password
-    debug(f"Username is {username}, and Password is {password}")
     baseURL = args.baseURL
+    debug(f"Username is {username}, and Password is {password}")
     debug(f"Base URL is {baseURL}")
+
+    project_list = get_projects(baseURL, username, password)
+    debug(project_list)
+   
+    project_count = 0
+    all_repos_list = []
+    project_repos = []
+    for project in project_list:
+        debug(f"Project {project_count} is {project}")
+        
+        project_repos = get_repos(baseURL, username, password, project)
+        all_repos_list.extend(project_repos)
+        
+        project_count += 1
+        project_repos = []
+
+        debug(len(all_repos_list))
+
+    debug(f"All repos: {all_repos_list}")
+    total_repos = len(all_repos_list)
+    info(f"Number of repos: {total_repos}")
 
 if __name__ == '__main__':
     args = parse_arguments()
